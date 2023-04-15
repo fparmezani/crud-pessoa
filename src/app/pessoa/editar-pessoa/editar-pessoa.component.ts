6a04a9e54e1c5fcb2c3b76501d56cdd45ac65dbc
Fernando <fparmezani@hotmail.com>
2023-04-15 10:33:36 -0300

    primeiro commit

---
+++ src/app/pessoa/editar-pessoa/editar-pessoa.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PessoaService } from '../services/pessoa.service';
+import { NgForm } from '@angular/forms';
+import { Pessoa } from 'src/app/shared/models/pessoa.model';
+
+@Component({
+  selector: 'app-editar-pessoa',
+  templateUrl: './editar-pessoa.component.html',
+  styleUrls: ['./editar-pessoa.component.css']
+})
+export class EditarPessoaComponent implements OnInit {
+
+  @ViewChild("formPessoa") formPessoa!: NgForm;
+  pessoa!: Pessoa;
+
+  constructor(private pessoaService: PessoaService,
+    private route: ActivatedRoute,
+    private router: Router) {
+
+  }
+
+  ngOnInit(): void {
+
+    let id = +this.route.snapshot.params['id'];
+
+    const res = this.pessoaService.buscarPorId(id);
+
+    if (res !== undefined) {
+      this.pessoa = res;
+    } else {
+      throw new Error("Pessoa nao encontrada: id" + id);
+    }
+
+  }
+
+  atualizar(): void {
+
+    if (this.formPessoa.form.valid) {
+      this.pessoaService.atualizar(this.pessoa);
+      this.router.navigate(['/pessoas']);
+    }
+
+  }
+
+}
---
+++ src/app/pessoa/listar-pessoa/listar-pessoa.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Pessoa } from 'src/app/shared/models/pessoa.model';
+import { PessoaService } from '../services/pessoa.service';
+
+@Component({
+  selector: 'app-listar-pessoa',
+  templateUrl: './listar-pessoa.component.html',
+  styleUrls: ['./listar-pessoa.component.css']
+})
+export class ListarPessoaComponent implements OnInit {
+
+  pessoas: Pessoa[] = [];
+
+  constructor(private pessoaService: PessoaService) { }
+
+  ngOnInit(): void {
+    this.pessoas = this.listarTodos();
+  }
+
+  listarTodos(): Pessoa[] {
+    return this.pessoaService.listarTodos();
+  }
+
+  remover($event: any, pessoa: Pessoa): void {
+
+    $event.preventDefault();
+    if (confirm(`Deseja ealmente remover a pessoa ${pessoa.nome}>
+    ?`)) {
+
+      this.pessoaService.remover(pessoa.id!);
+      this.pessoas = this.listarTodos();
+    }
+
+  }
+
+}
